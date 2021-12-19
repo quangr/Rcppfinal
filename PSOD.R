1e13769bd38c2ae14cec947c65ab510de16f8285
quangr <quangr@163.com>
2021-12-19 19:00:08 +0800

    加入lip距离，修改测试函数

--- PSOD.R
+++ PSOD.R
@@ -46,6 +46,8 @@ PSOD.common.constraint2 <- function(n, m) {
   map(1:n,~diag(m))%>%reduce(cbind)%>%unname()
 }
 
+
+
 # Finite dimensional q-space
 PSOD.Finite.qnorm <- function(data, q = 1, formula=~., m = 2) {
   design <- model.matrix(formula, data = data)
@@ -90,11 +92,81 @@ PSOD.Finite.qnorm <- function(data, q = 1, formula=~., m = 2) {
 
 }
 
+
+
+# constraint matrix 1, input:X(n*r), return a constraint matrix A((2^r)*(np))
+PSOD.Lipschitz.constraint1 <- function(n, m) {
+  combo <- map(1:(m - 1), ~ cross2(c(.x), (.x + 1):m)) %>% flatten()
+  comat=map(1:n,function(x){tempv=map(1:n,~.x==x)%>%flatten_dbl();return(rep(tempv,each=n)-rep(tempv,n))})
+  combonum=choose(m,2)
+  commatrix <- imap(combo, function(v,comboindex) {
+    t <- rep(0, m)
+    t[v[[1]]] <- 1
+    t[v[[2]]] <- (-1)
+    tempvec=t%>%matrix(1,m)
+    imap(comat,function(mat,i){
+      sconstrain=c(rep(0,n*n*(comboindex-1)),mat,rep(0,n*n*(combonum-comboindex)))
+      c(rep(0,length(tempvec)*(i-1)),-tempvec,rep(0,length(tempvec)*(n-i)),sconstrain)
+    })%>%reduce(rbind)%>%unname()
+  }) %>% reduce(rbind)%>%unname()
+
+  commatrix
+}
+PSOD.Lipschitz.constraint2 <- function(distm, m) {
+  distv=distm%>%as.list()%>%flatten_dbl()
+  n=ncol(distm)
+  combonum=choose(m,2)
+  commatrix <- map(1:combonum, function(comboindex) {
+      sconstrain=c(rep(0,n*n*(comboindex-1)),-distv,rep(0,n*n*(combonum-comboindex)))
+      c(rep(0,nrow(distm)*m),sconstrain)
+  }) %>% reduce(rbind)%>%unname()
+  
+  commatrix%>%matrix(ncol=combonum*n*n+m*n)
+}
+
+
+
+PSOD.common.constraint1 <- function(n, m) {
+  map(1:n,~c(rep(0,(.x-1)*m),rep(1,m),rep(0,(n-.x)*m))%>%reduce(cbind))%>%reduce(rbind)%>%unname()
+}
+
+PSOD.common.constraint2 <- function(n, m) {
+  map(1:n,~diag(m))%>%reduce(cbind)%>%unname()
+}
+
 # Lipschitz functions
 
 
-PSOD.Lipschitz <- function(data, X, q = 1) {
+PSOD.Lipschitz <- function(data,m = 2,distance="euclidean") {
+  distm <- as.matrix(dist(data,method = "euclidean"))
+  n=nrow(data)
+  
+  CCL1=PSOD.common.constraint1(n,m)
+  CCL2=PSOD.common.constraint2(n,m)
+  
+  if(m>1){
+    CCL1=cbind(rep(0,nrow(CCL1)),CCL1,matrix(rep(0,n*n*choose(m,2)*nrow(CCL1)),nrow(CCL1)))
+    CCL1=L_constraint(CCL1,dir=rep("==",nrow(CCL1)),rhs=rep(1,nrow(CCL1)))
 
+    
+    CCL2=cbind(rep(0,nrow(CCL2)),CCL2,matrix(rep(0,n*n*choose(m,2)*nrow(CCL2)),nrow(CCL2)))
+    CCL2=L_constraint(CCL2,dir=rep("==",nrow(CCL2)),rhs=rep(n/m,nrow(CCL2)))
+    CL1=PSOD.Lipschitz.constraint1(n,m)
+    CL1=cbind(rep(0,nrow(CL1)),CL1)
+    CL1=L_constraint(CL1,dir=rep("==",nrow(CL1)),rhs=rep(0,nrow(CL1)))
+
+    CL2=PSOD.Lipschitz.constraint2(distm,m)
+    CL2=cbind(rep(1,nrow(CL2)),CL2)
+    CL2=L_constraint(CL2,dir=rep(">=",nrow(CL2)),rhs=rep(0,nrow(CL2)))
+    
+    types = c("C",rep("B", m*n),rep("C", choose(m,2)*n*n))
+    milp <- OP(objective = L_objective(c(1, rep(0, m*n+choose(m,2)*n*n))),
+               constraints = c(CL2,CL1,CCL1,CCL2),
+               types =types, 
+               maximum = FALSE)
+    (sol <- ROI_solve(milp))
+    return(solution(sol)[2:(2+m*n-1)])
+  }
 }
 
 
--- SimulationExperiment.R
+++ SimulationExperiment.R
@@ -1,18 +1,31 @@
 library(ROI)
 library(purrr)
 library(furrr)
+library(ggplot2)
 future::plan(multisession)
 source("./PSOD.R", local = T)
+pairdata=function(n,m=2){
+  test=data.frame(x=rnorm(n),y=rnorm(n))
+  map(1:(m-1),~test+data.frame(x=rnorm(n,sd = 0.05),y=rnorm(n,sd = 0.05)))%>%reduce(rbind,.init = test)
+}
+
 RunSimulation=function(){
   future_map(1:10,function(i){
     test=data.frame(x=rnorm(10),y=rnorm(10))
     test=test%>%rbind(test+data.frame(x=rnorm(10,sd = 0.05),y=rnorm(10,sd = 0.05)))
-    ar1=matrix(PSOD.Finite.qnorm(test),ncol = 2,byrow = T)%*%c(1,2)%>%as.vector()
-    ar2=matrix(PSOD.Finite.qnorm(test,q=Inf),ncol = 2,byrow = T)%*%c(1,2)%>%as.vector()
+    ar1=matrix(PSOD.Finite.qnorm(test),ncol = 2,byrow = T)%*%1:2%>%as.vector()
+    ar2=matrix(PSOD.Finite.qnorm(test,q=Inf),ncol = 2,byrow = T)%*%1:2%>%as.vector()
     ar3=rep(c(1,2),each=10)
     treat1=test$x-test$y+1/2
     treat2=test$x-test$y-1/2
     map(list(ar1,ar2,ar3),~mean(treat1[.x==1]-treat2[.x==2]))
   })%>%transpose()%>%map(~var(unlist(.x)))%>%unlist
   
+}
+
+
+PlotResult=function(m){
+  paridata=pairdata(50,m)
+  ar1=matrix(PSOD.Lipschitz(paridata,m=m),ncol = m,byrow = T)%*%1:m%>%as.vector()
+  ggplot(paridata,aes(x=x,y=y,shape=as.factor(ar1)))+geom_point()
 }
