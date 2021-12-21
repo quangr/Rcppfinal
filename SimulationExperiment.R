c0b2bc5752a2d7541207bf9e3fe242b50ccd1774
quangr <quangr@163.com>
2021-12-21 13:48:05 +0800

    加入测试函数

--- SimulationExperiment.R
+++ SimulationExperiment.R
@@ -8,24 +8,34 @@ pairdata=function(n,m=2){
   test=data.frame(x=rnorm(n),y=rnorm(n))
   map(1:(m-1),~test+data.frame(x=rnorm(n,sd = 0.05),y=rnorm(n,sd = 0.05)))%>%reduce(rbind,.init = test)
 }
-
-RunSimulation=function(){
-  future_map(1:10,function(i){
-    test=data.frame(x=rnorm(10),y=rnorm(10))
-    test=test%>%rbind(test+data.frame(x=rnorm(10,sd = 0.05),y=rnorm(10,sd = 0.05)))
-    ar1=matrix(PSOD.Finite.qnorm(test),ncol = 2,byrow = T)%*%1:2%>%as.vector()
-    ar2=matrix(PSOD.Finite.qnorm(test,q=Inf),ncol = 2,byrow = T)%*%1:2%>%as.vector()
-    ar3=rep(c(1,2),each=10)
-    treat1=test$x-test$y+1/2
-    treat2=test$x-test$y-1/2
-    map(list(ar1,ar2,ar3),~mean(treat1[.x==1]-treat2[.x==2]))
-  })%>%transpose()%>%map(~var(unlist(.x)))%>%unlist
-  
+uniformdata=function(n){
+  test=data.frame(x=runif(n,-1),y=runif(n,-1))
+  test
 }
 
+RunSimulation=function(functionset,repeatnum=20,datagen=uniformdata,m=2){
+  future_map(1:repeatnum,function(i){
+    testseq=seq(m,30,m*2)
+    data=datagen(testseq[length(testseq)])
+    arrresult=map(functionset,~map(testseq,function(i){.x(data[1:i,])}))
+    list(data=data,arrange=arrresult)
+  })
+}
 
-PlotResult=function(m){
-  data=pairdata(30,m)
+result=RunSimulation(list(PSOD.Finite.qnorm,partial(PSOD.Finite.qnorm,q=Inf),PSOD.RKHS))
+
+PlotResult=function(result,treatset){
+   data=pairdata(30,m)
   ar1=matrix(PSOD.Lipschitz(data,m=m),ncol = m,byrow = T)%*%1:m%>%as.vector()
   ggplot(data,aes(x=x,y=y,shape=as.factor(ar1)))+geom_point()
 }
+PlotCompare=function(result,treatmentfun=function(x,y){x+y}){
+  curve=result%>%map(~map(.x$arrange,function(ars){
+    treat1=treatmentfun(.x$data$x,.x$data$y)+1
+    treat2=treatmentfun(.x$data$x,.x$data$y)
+    map(ars,~mean(treat1[.x==1]-treat2[.x==2]))
+  }))%>%transpose()%>%map(transpose)%>%map(~map(.x,~var(unlist(.x))))%>%map(unlist)
+  reduce2(curve,1:length(curve),function(x,y,z){x+geom_line(mapping = aes(x = factor(1:length(y)), y = y,group=z))},.init = ggplot())
+}
+
+
